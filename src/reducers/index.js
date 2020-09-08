6ab3cd7ede3c7dfe7cea27612e3f03b5eb8daec5
Jordi-LG <jordy.leguet@gmail.com>
2020-09-08 17:11:19 +0200

    feat: main structure of react app

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux"
+
+// combineReducer permet d'aggréger plusieurs sous-reducers
+// en un reducer global : on parle de rootReducer
+export default combineReducers({})
