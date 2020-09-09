fb212c059889af3af993598ad814549d6655d859
Florianargaud <argaud.florian@gmail.com>
2020-09-09 10:46:00 +0200

    feat: Logo added with transform effect

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,10 +3,14 @@ import "./Nav.css"
 
 const Nav = () => (
   <div className="Nav bg-gray-900 h-full w-screen ">
-    <ul className="uppercase py-16 text-gray-100 text-xl flex justify-evenly">
-      <li>Créer</li>
+    <ul className="uppercase py-16 text-gray-100 text-xl flex justify-evenly items-center font-bold">
+      <li className="">Créer</li>
       <li>Jouer</li>
-      <li>LOGO</li>
+      <img
+        src="/assets/img/logo.png"
+        className="-my-10 transform duration-200 ease-in-out h-64 hover:scale-110 "
+        alt=""
+      />
       <li>A propos</li>
       <li>Contact</li>
     </ul>
