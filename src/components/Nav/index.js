e9e45fd93eb23ca784ac3665dc41cc726f0056e0
Jordi-LG <jordy.leguet@gmail.com>
2020-09-09 14:44:36 +0200

    feat: dynamic nav + order for img redirect to home

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,28 +1,27 @@
 import React from "react"
 import "./Nav.css"
 import { NavLink } from "react-router-dom"
+import navDatas from "src/datas/nav"
 
 const Nav = () => (
   <div className="nav bg-gray-900 h-full w-screen ">
     <ul className="uppercase py-16 text-gray-100 text-xl flex justify-evenly items-center font-bold">
-      <NavLink activeClassName="nav__selected" to="/create" className="">
-        Créer
-      </NavLink>
-      <NavLink activeClassName="nav__selected" to="/play">
-        Jouer
-      </NavLink>
-      <NavLink to="/">
+      {navDatas.map((item) => (
+        <NavLink
+          activeClassName="nav__selected"
+          to={item.route}
+          exact
+          className={`order-${item.order} p-5`}
+        >
+          {item.name}
+        </NavLink>
+      ))}
+      <NavLink to="/" className={`order-${Math.ceil(navDatas.length / 2)}`}>
         <img
           src="/assets/img/logo.png"
-          className="-my-10 transform duration-200 ease-in-out h-64 hover:scale-110 "
+          className="nav__logo -my-10 transform duration-200 ease-in-out h-64 hover:scale-110 "
           alt=""
         />
-      </NavLink>
-      <NavLink activeClassName="nav__selected" to="/about">
-        A propos
-      </NavLink>
-      <NavLink activeClassName="nav__selected" to="/contact">
-        Contact
       </NavLink>
     </ul>
   </div>
---
+++ src/datas/nav.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    order: 1,
+    name: "create",
+    route: "/create",
+  },
+  {
+    order: 2,
+    name: "play",
+    route: "/play",
+  },
+  {
+    order: 3,
+    name: "about",
+    route: "/about",
+  },
+  {
+    order: 4,
+    name: "contact",
+    route: "/contact",
+  },
+]
