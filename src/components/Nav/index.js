46bf0ebc834625b03f2560ba524aaa80396ade38
Jordi-LG <jordy.leguet@gmail.com>
2020-09-09 14:15:19 +0200

    feat: add NavLink components in Nav + activeClassName css

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,29 @@
 import React from "react"
 import "./Nav.css"
+import { NavLink } from "react-router-dom"
 
 const Nav = () => (
-  <div className="Nav bg-gray-900 h-full w-screen ">
+  <div className="nav bg-gray-900 h-full w-screen ">
     <ul className="uppercase py-16 text-gray-100 text-xl flex justify-evenly items-center font-bold">
-      <li className="">Créer</li>
-      <li>Jouer</li>
-      <img
-        src="/assets/img/logo.png"
-        className="-my-10 transform duration-200 ease-in-out h-64 hover:scale-110 "
-        alt=""
-      />
-      <li>A propos</li>
-      <li>Contact</li>
+      <NavLink activeClassName="nav__selected" to="/create" className="">
+        Créer
+      </NavLink>
+      <NavLink activeClassName="nav__selected" to="/play">
+        Jouer
+      </NavLink>
+      <NavLink to="/">
+        <img
+          src="/assets/img/logo.png"
+          className="-my-10 transform duration-200 ease-in-out h-64 hover:scale-110 "
+          alt=""
+        />
+      </NavLink>
+      <NavLink activeClassName="nav__selected" to="/about">
+        A propos
+      </NavLink>
+      <NavLink activeClassName="nav__selected" to="/contact">
+        Contact
+      </NavLink>
     </ul>
   </div>
 )
