67bab6febfe6975461d8c59c9a36cbeed4865898
Florianargaud <argaud.florian@gmail.com>
2020-09-09 11:17:03 +0200

    feat: Card display example

--- src/components/Create/CreateChoice.js
+++ src/components/Create/CreateChoice.js
@@ -2,7 +2,17 @@ import React from "react"
 import "./Create.css"
 
 const CreateChoice = () => (
-  <div className="Create bg-gray-300 shadow-lg h-64 w-full ">BARE DE SELECTION</div>
+  <div className="Create bg-gray-300 shadow-lg  flex py-8 w-full ">
+    <div className="w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl ease-in-out duration-200 transform hover:-translate-y-3">
+      LIEU
+    </div>
+    <div className="w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl ease-in-out duration-200 transform hover:-translate-y-3">
+      LIEU
+    </div>
+    <div className="w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl ease-in-out duration-200 transform hover:-translate-y-3">
+      LIEU
+    </div>
+  </div>
 )
 
 export default CreateChoice
