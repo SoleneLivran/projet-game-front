59848d887a2f46e967338858cecdbe75030eb034
Florianargaud <argaud.florian@gmail.com>
2020-09-09 10:34:44 +0200

    feat: Navbar Componnent

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./App.css"
 
-const App = () => <div className="App"> Hello </div>
+const App = () => <div className="App bg-gray-900 h-full w-screen "> Hello </div>
 
 export default App
---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./Nav.css"
+
+const Nav = () => (
+  <div className="Nav bg-gray-900 h-full w-screen ">
+    <ul className="uppercase flex justify-evenly">
+      <li>Créer</li>
+      <li>Jouer</li>
+      <li>LOGO</li>
+      <li>A propos</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+)
+
+export default App
