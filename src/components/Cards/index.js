b59787497b1cc1f99e42cfda3fb56f2c536af490
Florianargaud <argaud.florian@gmail.com>
2020-09-09 12:59:58 +0200

    feat: WIP Implement Cards DnD system

---
+++ src/components/Cards/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Cards.css"
+
+const Cards = (props) => {
+  return props.lieux.map((lieu) => (
+    <div className=" Dragable w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl ease-in-out duration-200 transform hover:-translate-y-3">
+      {props.lieux.content}
+    </div>
+  ))
+}
+
+export default Cards
--- src/components/Create/CreateChoice.js
+++ src/components/Create/CreateChoice.js
@@ -1,18 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Create.css"
+import { render } from "react-dom"
+import Cards from "src/components/Cards"
+// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 
-const CreateChoice = () => (
-  <div className="Create bg-gray-300 shadow-lg  flex py-8 w-full ">
-    <div className="w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl ease-in-out duration-200 transform hover:-translate-y-3">
-      LIEU
+// fake data
+const cardsNames = {
+  // Our dragable items
+  lieux: {
+    "lieu-1": { id: 0, content: "Forêt" },
+    "lieu-2": { id: 1, content: "Desert" },
+    "lieu-3": { id: 2, content: "Taverne" },
+  },
+  // Column where the items are curently in
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "select card",
+      // We declare the ownership : lieu-1 is currently in column-1
+      lieuIDs: ["lieu-1", "lieu-2", "lieu-3"],
+    },
+    // We define the column order
+    columnOrder: ["column-1"],
+  },
+}
+
+const CreateChoice = () => {
+  const lieuState = cardsNames
+
+  const cards = () => {
+    return lieuState.columns.columnOrder.map((columnID) => {
+      const column = lieuState.columns[columnID]
+      const lieux = column.lieuIDs.map((lieuID) => lieuState.lieux[lieuID])
+      return <Cards key={column.id} column={column} lieux={lieux} />
+    })
+  }
+  return (
+    <div className="DragDropContext Create bg-gray-300 shadow-lg  flex py-8 w-full ">
+      <div className="Dropable flex">{cards()}</div>
     </div>
-    <div className="w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl ease-in-out duration-200 transform hover:-translate-y-3">
-      LIEU
-    </div>
-    <div className="w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl ease-in-out duration-200 transform hover:-translate-y-3">
-      LIEU
-    </div>
-  </div>
-)
+  )
+}
 
 export default CreateChoice
