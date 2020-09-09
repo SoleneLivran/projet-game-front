0144d8b05868441d11c998a624e5a11892471914
Florianargaud <argaud.florian@gmail.com>
2020-09-09 23:41:39 +0200

    feat: POC DnD System WIP

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,12 +1,27 @@
 import React from "react"
 import "./Cards.css"
+import { Draggable } from "react-beautiful-dnd"
 
 const Cards = (props) => {
-  return props.lieux.map((lieu) => (
-    <div className=" Dragable w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl ease-in-out duration-200 transform hover:-translate-y-3">
-      {lieu.content}
-    </div>
+  return props.lieux.map((lieu, index) => (
+    <Draggable draggableId={lieu.id} index={index}>
+      {(provided, index) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className=" Dragable w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl ease-in-out duration-200 transform hover:-translate-y-3"
+        >
+          {lieu.content}
+        </div>
+      )}
+    </Draggable>
   ))
 }
 
 export default Cards
+
+/**
+ *
+ *
+ */
--- src/components/Create/CreateChoice.js
+++ src/components/Create/CreateChoice.js
@@ -1,46 +1,117 @@
-import React, { useState } from "react"
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 import "./Create.css"
-import { render } from "react-dom"
-import Cards from "src/components/Cards"
-// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 
-// fake data
-const cardsNames = {
-  // Our dragable items
-  lieux: {
-    "lieu-1": { id: 0, content: "Forêt" },
-    "lieu-2": { id: 1, content: "Desert" },
-    "lieu-3": { id: 2, content: "Taverne" },
-    "lieu-4": { id: 3, content: "Espace" },
-  },
-  // Column where the items are curently in
-  columns: {
-    "column-1": {
-      id: "column-1",
-      title: "select card",
-      // We declare the ownership : lieu-1 is currently in column-1
-      lieuIDs: ["lieu-1", "lieu-2", "lieu-3", "lieu-4"],
-    },
-    // We define the column order
-    columnOrder: ["column-1"],
-  },
+const getItems = (count) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `item-${k}`,
+    content: `Lieu ${k}`,
+    description: `Description du Lieu`,
+  }))
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
 }
 
-const CreateChoice = () => {
-  const lieuState = cardsNames
+const grid = 8
 
-  const cards = () => {
-    return lieuState.columns.columnOrder.map((columnID) => {
-      const column = lieuState.columns[columnID]
-      const lieux = column.lieuIDs.map((lieuID) => lieuState.lieux[lieuID])
-      return <Cards key={column.id} column={column} lieux={lieux} />
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  display: "flex",
+  padding: grid,
+  overflow: "auto",
+})
+
+class CreateChoice extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: getItems(6),
+    }
+    this.onDragEnd = this.onDragEnd.bind(this)
+  }
+
+  id2List = {
+    droppable: "items",
+    droppable2: "selected",
+  }
+
+  onDragEnd(result) {
+    if (!result.destination) {
+      return
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    )
+
+    this.setState({
+      items,
     })
   }
-  return (
-    <div className="DragDropContext Create bg-gray-300 shadow-lg  flex py-8 w-full ">
-      <div className="Dropable flex">{cards()}</div>
-    </div>
-  )
+
+  render() {
+    return (
+      <div>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <div className="flex justify-center mx-auto">
+            <Droppable droppableId="droppable" direction="horizontal">
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  className="select-none my-4 w-48 h-48 rounded-lg bg-gray-200 mx-8 flex justify-center border-4 border-dashed border-gray-500 items-center text-gray-500 font-bold text-2xl"
+                >
+                  LIEU
+                </div>
+              )}
+            </Droppable>
+            <div className="select-none my-4 w-48 h-48 rounded-lg bg-gray-200 mx-8 flex justify-center border-4 border-dashed border-gray-500 items-center text-gray-500 font-bold text-2xl">
+              EVENEMENT
+            </div>
+            <div className="select-none my-4 w-48 h-48 rounded-lg bg-gray-200 mx-8 flex justify-center border-4 border-dashed border-gray-500 items-center text-gray-500 font-bold text-2xl">
+              ACTIONS
+            </div>
+          </div>
+          <Droppable droppableId="droppable" direction="horizontal">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                className="flex p-8"
+                {...provided.droppableProps}
+              >
+                {this.state.items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        className="select-none w-48 h-64 rounded-lg bg-gray-800 mx-8 flex justify-center items-center text-white font-bold text-2xl transform hover:-translate-y-3"
+                      >
+                        <div className="flex flex-col">
+                          <p> {item.content}</p>
+                          <p className="text-lg font-normal">{item.description}</p>
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+    )
+  }
 }
 
 export default CreateChoice
