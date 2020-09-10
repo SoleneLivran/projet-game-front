4fd6bc6d74f4dc0a93d40dfcbb05b374d88ebda6
Jordi-LG <jordy.leguet@gmail.com>
2020-09-10 19:17:01 +0200

    feat: create action save popular stories

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,5 +1,10 @@
 export const FETCH_POPULAR_STORIES = "FETCH_POPULAR_STORIES"
+export const SAVE_POPULAR_STORIES = "SAVE_POPULAR_STORIES"
 
 export const fetchPopularStories = () => ({
   type: FETCH_POPULAR_STORIES,
+})
+
+export const savePopularStories = () => ({
+  type: SAVE_POPULAR_STORIES,
 })
--- src/reducers/app.js
+++ src/reducers/app.js
@@ -5,6 +5,12 @@ export const initialState = {
 
 const app = (state = initialState, action = {}) => {
   switch (action.type) {
+    case "SAVE_POPULAR_STORIES": {
+      console.log("SAVE POPULAR STORIES");
+      return {
+        ...state,
+      }
+    }
     default:
       return state
   }
