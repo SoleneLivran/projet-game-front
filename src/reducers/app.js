357b0346acd8e46f1c8af80994367fea5e02ce12
Jordi-LG <jordy.leguet@gmail.com>
2020-09-10 19:03:03 +0200

    feat: create app reducer

---
+++ src/reducers/app.js
@@ -0,0 +1,11 @@
+export const initialState = {
+  listPopular: [],
+  listLastest: [],
+}
+
+const app = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
