5b2eb7b8471fb49bef3ea3c8bbf22bb166212079
Jordi-LG <jordy.leguet@gmail.com>
2020-09-10 20:12:33 +0200

    feat: add axios, WIP request the API url because CORS error

--- src/middleware/app.js
+++ src/middleware/app.js
@@ -1,9 +1,24 @@
+import axios from "axios"
 import { FETCH_POPULAR_STORIES, savePopularStories } from "src/actions/app"
 
 const app = (store) => (next) => (action) => {
   switch (action.type) {
     case FETCH_POPULAR_STORIES: {
       console.log("Middleware: Je fetch les stories popular")
+      axios
+        .get(
+          "http://ec2-18-234-186-84.compute-1.amazonaws.com/projet-jeu/gwith_back/public/api/stories",
+          {
+            headers: {
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error);
+        })
       store.dispatch(savePopularStories())
     }
     default:
