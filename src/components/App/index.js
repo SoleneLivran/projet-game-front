acfc44e9085231baba6a60ffa05c5e0a67628993
Jordi-LG <jordy.leguet@gmail.com>
2020-09-10 18:42:13 +0200

    feat: create App container and connect to App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { useEffect } from "react"
+import PropTypes from "prop-types"
 import "./App.css"
 import Nav from "src/components/Nav/index"
 import Home from "src/components/Home/index"
 
-const App = () => (
-  <div className="app bg-center bg-cover h-auto w-screen ">
-    <Nav />
-    <Home />
-  </div>
-)
+const App = ({ fetchPopularStories }) => {
+  useEffect(() => {
+    fetchPopularStories()
+  }, [])
 
+  return (
+    <div className="app bg-center bg-cover h-auto w-screen ">
+      <Nav />
+      <Home />
+    </div>
+  )
+}
+
+App.propTypes = {
+  fetchPopularStories: PropTypes.func.isRequired,
+}
 export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "src/styles/index.css"
-import App from "src/components/App"
+import App from "src/containers/App/index"
 import store from "src/store"
 import { Provider } from "react-redux"
 import * as serviceWorker from "./serviceWorker"
