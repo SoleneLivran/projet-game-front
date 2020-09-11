92551ef7a81b2b8b6ff567d6d4d2782ca8202dff
Jordi-LG <jordy.leguet@gmail.com>
2020-09-11 14:13:58 +0200

    feat: prepare proptypes in home components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,9 @@
 import React from "react"
+import PropTypes from "prop-types"
 import "./styles.css"
 import Card from "src/components/Card/index"
 
-const Home = () => (
+const Home = (popularStories) => (
   <div className="home md:mt-32">
     <section className="home__stories mx-8 my-16">
       <h1 className="home__title uppercase text-white text-3xl font-bold">
@@ -10,23 +11,20 @@ const Home = () => (
       </h1>
       <section className="home__popular mb-12 mt-4">
         <ul className="home__list-popular">
-        <Card />
-          <li className="home_item">Story 2</li>
-          <li className="home_item">Story 3</li>
+          <Card />
         </ul>
       </section>
       <h1 className="home__title uppercase text-white text-3xl font-bold">
         Nouveautés
       </h1>
       <section className="home__latest">
-        <ul className="home__list-latest">
-          <li className="home_item">Story 1</li>
-          <li className="home_item">Story 2</li>
-          <li className="home_item">Story 3</li>
-        </ul>
+        <ul className="home__list-latest"></ul>
       </section>
     </section>
   </div>
 )
 
+Home.propTypes = {
+  popularStories: PropTypes.array.isRequired,
+}
 export default Home
