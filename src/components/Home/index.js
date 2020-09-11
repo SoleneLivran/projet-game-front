d1049de4222de767085a7ba9f32074270b9b46d5
Jordi-LG <jordy.leguet@gmail.com>
2020-09-11 14:53:58 +0200

    feat: display data in Card component done, keep html card example

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import "./styles.css"
 import Card from "src/components/Card/index"
 
-const Home = (popularStories) => (
+const Home = ({ popularStories }) => (
   <div className="home md:mt-32">
     <section className="home__stories mx-8 my-16">
       <h1 className="home__title uppercase text-white text-3xl font-bold">
@@ -11,7 +11,9 @@ const Home = (popularStories) => (
       </h1>
       <section className="home__popular mb-12 mt-4">
         <ul className="home__list-popular">
-          <Card />
+          {popularStories.map((story) => (
+            <Card key={story.id} {...story} />
+          ))}
         </ul>
       </section>
       <h1 className="home__title uppercase text-white text-3xl font-bold">
