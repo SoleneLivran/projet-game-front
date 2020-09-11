4d7d87d78a3561ea00143870c1e1ee44deaa19e8
Jordi-LG <jordy.leguet@gmail.com>
2020-09-11 18:59:12 +0200

    feat: modal appear and close when outside click

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,37 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import "./styles.css"
 import Card from "src/components/Card/index"
 import UserPanel from "src/components/UserPanel/index"
+import Modal from "src/components/Modal/index"
 
-const Home = ({ popularStories }) => (
-  <div className="home md:mt-32 mx-8 md:flex md:my-16 md:justify-around">
-    <section className="home__user-mobile hidden mt-8 sm:block md:hidden">
-      <UserPanel />
-    </section>
-    <section className="home__stories my-16 sm:my-8 md:w-2/3 md:my-0 md:mr-12">
-      <h1 className="home__title uppercase text-white text-3xl font-bold">
-        Populaires
-      </h1>
-      <section className="home__popular mb-12 mt-4">
-        <ul className="home__list-popular">
-          {popularStories.map((story) => (
-            <Card key={story.id} {...story} />
-          ))}
-        </ul>
+const Home = ({ popularStories }) => {
+  // Modal state, display on button or hiddne when closing it
+  const [showModal, setModal] = useState(false)
+  const handleModal = () => setModal(true)
+
+  return (
+    <div className="home md:mt-32 mx-8 md:flex md:my-16 md:justify-around">
+      {showModal && <Modal showModal={showModal} onClose={() => setModal(false)} />}
+      <button
+        onClick={() => handleModal()}
+        className="w-16 h-16 bg-blue-500"
+        type="button"
+      >
+        Button
+      </button>
+      <section className="home__user-mobile hidden mt-8 sm:block md:hidden">
+        <UserPanel />
       </section>
-      <h1 className="home__title uppercase text-white text-3xl font-bold">
-        Nouveautés
-      </h1>
-      <section className="home__latest">
-        <ul className="home__list-latest"></ul>
+      <section className="home__stories my-16 sm:my-8 md:w-2/3 md:my-0 md:mr-12">
+        <h1 className="home__title uppercase text-white text-3xl font-bold">
+          Populaires
+        </h1>
+        <section className="home__popular mb-12 mt-4">
+          <ul className="home__list-popular">
+            {popularStories.map((story) => (
+              <Card key={story.id} {...story} />
+            ))}
+          </ul>
+        </section>
+        <h1 className="home__title uppercase text-white text-3xl font-bold">
+          Nouveautés
+        </h1>
+        <section className="home__latest">
+          <ul className="home__list-latest"></ul>
+        </section>
       </section>
-    </section>
-    <section className="home__user-desktop hidden mt-24 md:block md:w-1/3">
-      <UserPanel />
-    </section>
-  </div>
-)
+      <section className="home__user-desktop hidden mt-24 md:block md:w-1/3">
+        <UserPanel />
+      </section>
+    </div>
+  )
+}
 
 Home.propTypes = {
   popularStories: PropTypes.array.isRequired,
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,10 +1,31 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 import "./styles.css"
 
-const Modal = () => 
-<div className="modal h-64 w-64">
-  <h1 className="modal__title">Title</h1>
-  <p className="modal__synopsis">Synopsis</p>
-</div>
+const Modal = ({ showModal, onClose }) => {
+  const displayModal = showModal === true ? "block" : "hidden"
+  const ref = useRef(null)
+  useEffect(() => {
+    document.addEventListener("click", clickListener)
+    return () => {
+      document.removeEventListener("click", clickListener)
+    }
+  }, [])
+
+  const clickListener = (e) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      onClose()
+    }
+  }
+
+  return (
+    <div ref={ref} className={`modal h-64 w-64 bg-gray-100 ${displayModal}`}>
+      <button className="modal__close h-8 w-8 bg-red-200" type="button">X</button>
+      <div className="modal__content">
+        <h1 className="modal__title">Title</h1>
+        <p className="modal__synopsis">Synopsis</p>
+      </div>
+    </div>
+  )
+}
 
 export default Modal
