adab4e7e091bc245cffc6754b43c9171603167db
Jordi-LG <jordy.leguet@gmail.com>
2020-09-11 10:54:02 +0200

    feat: user_panel base, WIP more style

--- src/components/UserPanel/index.js
+++ src/components/UserPanel/index.js
@@ -3,11 +3,21 @@ import "./styles.css"
 
 const UserPanel = () => (
   <div className="user-panel">
+    <div className="h-24 w-full bg-gray-900 text-white text-lg font-bold flex items-center justify-between rounded-l-full">
     <div className="user-panel__img h-24 w-24 rounded-full bg-contain bg-center" />
-    <div>
-      <p className="user-panel__content h-24 w-full bg-gray-900 text-white text-lg font-bold">
-        Bienvenue, jeune aventurier.
-      </p>
+      <p className="user-panel__content">Bienvenue, jeune aventurier.</p>
+      <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Inscription
+      </button>
+      <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Connexion
+      </button>
     </div>
   </div>
 )
