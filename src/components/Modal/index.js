f299f4894fe5664417dc7c4943a2134d6f3df2fe
Jordi-LG <jordy.leguet@gmail.com>
2020-09-11 17:53:23 +0200

    feat: create modal + base structure

---
+++ src/components/Modal/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./styles.css"
+
+const Modal = () => 
+<div className="modal h-64 w-64">
+  <h1 className="modal__title">Title</h1>
+  <p className="modal__synopsis">Synopsis</p>
+</div>
+
+export default Modal
