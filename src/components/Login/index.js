dceacc325b8df26b9a2ccfa34dc26af7ea959c85
Jordi-LG <jordy.leguet@gmail.com>
2020-09-12 13:27:47 +0200

    feat: create component Field, use for Login input

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,43 +1,31 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./styles.css"
+import Field from "src/components/Field/index"
 
 const Login = () => (
   <div className="login mt-8 md:flex">
-    <div className="login__panel w-64 flex flex-col justify-center mx-auto p-4 border-2 border-orange-400 bg-orange-900 font-bold">
+    <form className="login__panel w-64 flex flex-col justify-center mx-auto p-4 border-2 border-orange-400 bg-orange-900 font-bold">
       <h1 className="login__title text-xl font-bold text-center text-orange-400">
         Prouvez que vous n'êtes pas n'importe quel guerrier.
       </h1>
       <label className="login__label mt-4 mb-2 text-orange-400" htmlFor="email">
         Email
       </label>
-      <input
-        className="login__input px-4 mb-2 h-12"
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Email"
-        required
-      />
+      <Field type="email" name="email" placeholder="Email" />
 
       <label className="login__label my-2 text-orange-400" htmlFor="password">
         Mot de passe
       </label>
-      <input
-        className="login__input px-4 mb-6 h-12"
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Mot de passe"
-        required
-      />
+      <Field type="password" name="password" placeholder="Mot de passe" />
 
       <input
-        className="login__submit cursor-pointer h-32 p-6 mt-2 uppercase font-bold whitespace-pre-wrap border-4 border-double border-yellow-400 bg-orange-600 rounded-sm shadow-inner"
+        className="login__submit cursor-pointer h-32 p-6 mt-8 uppercase font-bold whitespace-pre-wrap border-4 border-double border-yellow-400 bg-orange-600 rounded-sm shadow-inner"
         type="submit"
         value="Entrer dans la légende"
       />
-    </div>
+    </form>
+
     <section className="login__noaccount w-64 h-32 flex flex-col justify-center mx-auto p-4 border-2 border-orange-400 bg-orange-900 font-bold mt-4">
       <p className="login__signup text-center">Pas encore de compte ?</p>
       <Link to="/signup" className="mx-auto">
