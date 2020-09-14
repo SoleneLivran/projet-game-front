74ab839b79d8add9c2a8ea8c707ea2226a100ae2
Jordi-LG <jordy.leguet@gmail.com>
2020-09-14 12:01:20 +0200

    feat: structure SignupForm ok

--- src/components/SignupForm/index.js
+++ src/components/SignupForm/index.js
@@ -3,31 +3,56 @@ import "./styles.css"
 import Field from "src/components/Field/index"
 
 const SignupForm = () => (
-  <div className="signup-form">
+  <div className="signup-form mt-8">
     <form
       // onSubmit={(e) => {
       //   e.preventDefault()
       //   handleLogin()
       // }}
-      className="login-form__panel w-64 flex flex-col justify-center mx-auto p-4 border-2 border-orange-400 bg-orange-900 font-bold"
+      className="signup-form__panel w-64 flex flex-col justify-center mx-auto p-4 border-2 border-orange-400 bg-orange-900 font-bold"
     >
-      <h1 className="login-form__title text-xl font-bold text-center text-orange-400">
+      <h1 className="signup-form__title text-xl font-bold text-center text-orange-400">
         Inscription
       </h1>
-      <label className="login-form__label mt-4 mb-2 text-orange-400" htmlFor="email">
-        Email
+
+      <label
+        className="signup-form__label mt-4 mb-2 text-orange-400"
+        htmlFor="pseudo"
+      >
+        * Pseudo
+      </label>
+      <Field type="text" name="pseudo" placeholder="Pseudo" />
+
+      <label
+        className="signup-form__label mt-4 mb-2 text-orange-400"
+        htmlFor="email"
+      >
+        * Email
       </label>
       <Field type="email" name="email" placeholder="Email" />
 
-      <label className="login-form my-2 text-orange-400" htmlFor="password">
-        Mot de passe
+      <label className="signup-form my-2 text-orange-400" htmlFor="password">
+        * Mot de passe
       </label>
       <Field type="password" name="password" placeholder="Mot de passe" />
 
+      <label className="signup-form my-2 text-orange-400" htmlFor="password">
+        * Confirmez votre mot de passe
+      </label>
+      <Field
+        type="password"
+        name="password-check"
+        placeholder="Confirmation mot de passe"
+      />
+
+      <p className="signup-form__info italic text-xs mt-1">
+        * Informations obligatoires
+      </p>
+
       <input
-        className="login-form__submit cursor-pointer h-32 p-6 mt-8 uppercase font-bold whitespace-pre-wrap border-4 border-double border-yellow-400 bg-orange-600 rounded-sm shadow-inner"
+        className="signup-form__submit cursor-pointer h-32 p-6 mt-8 uppercase font-bold whitespace-pre-wrap border-4 border-double border-yellow-400 bg-orange-600 rounded-sm shadow-inner"
         type="submit"
-        value="Entrer dans la légende"
+        value="Confirmer l'inscription"
       />
     </form>
   </div>
