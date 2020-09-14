214acda9b73999a01f12edc0fbf55b026471e8fb
Jordi-LG <jordy.leguet@gmail.com>
2020-09-14 17:04:45 +0200

    feat: send data to API, return response 200

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,6 +6,21 @@ const auth = (store) => (next) => (action) => {
     case LOGIN: {
       // access data in store
       const state = store.getState()
+      const user = {
+        username: "Toto",
+        password: "toto",
+      }
+      axios
+        .post(
+          "http://ec2-18-234-186-84.compute-1.amazonaws.com/api/login_check",
+          user
+        )
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
       // axios'll come here
       // after receive the correct status response from POST request:
       store.dispatch(saveUser())
