2037b86b93fe069f7ca75b0334dd6f1489489649
Jordi-LG <jordy.leguet@gmail.com>
2020-09-14 18:49:32 +0200

    styles: fix somes styles for Home componant

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -14,7 +14,7 @@ const Home = ({ popularStories, fetchPopularStories }) => {
   const handleModal = () => setModal(true)
 
   return (
-    <div className="home md:mt-32 mx-8 md:flex md:my-16 md:justify-around">
+    <div className="home md:mt-32 mx-8 md:flex md:mt-16 md:justify-around">
       {/* Only when modal is open (true) -> Modal is display, and the event click/key can be use */}
       {showModal && <Modal showModal={showModal} onClose={() => setModal(false)} />}
 
@@ -23,20 +23,22 @@ const Home = ({ popularStories, fetchPopularStories }) => {
         <UserPanel />
       </section>
       <section className="home__stories my-16 sm:my-8 md:w-2/3 md:my-0 md:mr-12">
-        <h1 className="home__title uppercase text-white text-3xl font-bold">
-          Populaires
-        </h1>
         <section className="home__popular mb-12 mt-4">
+          <h1 className="home__title uppercase text-white text-3xl font-bold">
+            Populaires
+          </h1>
+
           <ul className="home__list-popular">
             {popularStories.map((story) => (
               <Card key={story.id} {...story} handleModal={handleModal} />
             ))}
           </ul>
         </section>
-        <h1 className="home__title uppercase text-white text-3xl font-bold">
-          Nouveautés
-        </h1>
         <section className="home__latest">
+          <h1 className="home__title uppercase text-white text-3xl font-bold">
+            Nouveautés
+          </h1>
+
           <ul className="home__list-latest"></ul>
         </section>
       </section>
