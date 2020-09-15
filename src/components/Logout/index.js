a27e8427b129e6ecd8664e39c27021ffec50836e
Jordi-LG <jordy.leguet@gmail.com>
2020-09-15 15:56:34 +0200

    feat: structure logout, ready to receive state from container

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,6 +1,12 @@
 import React from "react"
+import { Redirect } from "react-router-dom"
+import PropTypes from "prop-types"
 import "./styles.css"
 
-const Logout = () => <></>
+const Logout = ({ isLogged }) => <>{isLogged && <Redirect to="/login" />}</>
+
+Logout.propTypes = {
+  isLogged: PropTypes.func.isRequired,
+}
 
 export default Logout
