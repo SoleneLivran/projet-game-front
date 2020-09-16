9d8f080a35dba2108d840921ca881e1047789c78
Jordi-LG <jordy.leguet@gmail.com>
2020-09-16 15:44:53 +0200

    styles: fix if the story name is too long

--- src/components/Profile/Story/index.js
+++ src/components/Profile/Story/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Story = () => (
   <li className="stories__story mx-auto my-4 h-16 px-4 bg-gray-100 rounded-md flex justify-between items-center">
-    <h2 className="stories__story-title overflow-x-auto w-64">Nom de l'histoire</h2>
+    <h2 className="stories__story-title overflow-auto h-12 py-3">Nom de l'histoire</h2>
     <div className="stories__story-options flex justify-around items-center w-48">
       <p className="stories__story-status text-xs bg-green-500 h-8 px-1 py-2 rounded-md m-1 text-center">Publiée</p>
       {/* <p className="stories__story-status text-xs bg-blue-500 h-8 px-1 py-2 rounded-md m-1">Brouillon</p> */}
