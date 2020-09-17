2a53bf311fc90841be5746611048ac4e8f5a4723
Jordi-LG <jordy.leguet@gmail.com>
2020-09-17 17:44:38 +0200

    feat: receive user data from get request

--- src/middleware/user.js
+++ src/middleware/user.js
@@ -2,9 +2,24 @@ import axios from "axios"
 import { FETCH_USER } from "src/actions/user"
 
 const user = (store) => (next) => (action) => {
+  const state = store.getState()
   switch (action.type) {
     case FETCH_USER: {
-      console.log("Je suis dans la page user")
+      axios
+        .get(
+          `http://ec2-18-234-186-84.compute-1.amazonaws.com/api/app_users/${state.auth.connectedId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("user")}`,
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
     default:
       next(action)
