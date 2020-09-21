744159a4f352e2e89ae9feb5b87a24ed0218e683
Florianargaud <argaud.florian@gmail.com>
2020-09-21 18:59:46 +0200

    Create story with CRUD

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,7 @@ import Nav from "src/containers/Nav/index"
 import Home from "src/containers/Home/index"
 import LoginForm from "src/containers/LoginForm/index"
 import SignupForm from "src/containers/SignupForm/index"
+import Create from "src/components/Create/index"
 
 const App = () => (
   <div className="app bg-center bg-cover h-auto w-screen ">
@@ -13,6 +14,7 @@ const App = () => (
     <Route path="/" component={Home} exact />
     <Route path="/login" component={LoginForm} exact />
     <Route path="/signup" component={SignupForm} exact />
+    <Route path="/create" component={Create} exact />
   </div>
 )
 export default App
