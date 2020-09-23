14768707d84c1eccca140f11a1e733ad6e7d22e8
Florianargaud <argaud.florian@gmail.com>
2020-09-23 09:29:44 +0200

    feat: push build

---
+++ build/precache-manifest.24f8c2e77776659099d208136d1ff894.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78d4a8be51faeaa25c002d740fba87e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26f797feedd70d9427f9",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "63bb0b123a1399f6c064",
+    "url": "/static/css/main.ed7d55a0.chunk.css"
+  },
+  {
+    "revision": "26f797feedd70d9427f9",
+    "url": "/static/js/2.80709b69.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.80709b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bb0b123a1399f6c064",
+    "url": "/static/js/main.b12bc064.chunk.js"
+  },
+  {
+    "revision": "b30f1b9b25948d95c105",
+    "url": "/static/js/runtime-main.38086b43.js"
+  }
+]);
