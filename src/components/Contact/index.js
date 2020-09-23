382e86eb8c150bd5c0661e7bed2a5ea0609a9958
Jordi-LG <jordy.leguet@gmail.com>
2020-09-23 16:02:06 +0200

    feat: create component Contact

---
+++ src/components/Contact/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./styles.css"
+
+const Contact = () => (
+  // HTML entre les parenthèses. Bien compris dans la div parents "contact"
+  <div className="contact">
+    <h1 className="contact__title">Contact</h1>
+  </div>
+)
+
+export default Contact
