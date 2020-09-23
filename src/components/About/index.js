c637b50da4dd8b55bf08f331b84f7fd8934bdd23
Jordi-LG <jordy.leguet@gmail.com>
2020-09-23 16:06:07 +0200

    feat: create component About + routes about and contact

---
+++ src/components/About/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./styles.css"
+
+const About = () => (
+  <div className="about">
+    <h1 className="about__title">À propos</h1>
+  </div>
+)
+
+export default About
