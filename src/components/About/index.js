946ee27934031da28e0af8e8ff9e43c2ffaa404e
Jordi-LG <jordy.leguet@gmail.com>
2020-09-23 16:06:48 +0200

    comment: comment on about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import "./styles.css"
 
 const About = () => (
+  // HTML entre les parenthèses. Bien compris dans la div parents "about"
   <div className="about">
     <h1 className="about__title">À propos</h1>
   </div>
