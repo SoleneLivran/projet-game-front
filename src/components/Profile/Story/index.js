e92fc54affe8cc49423677a5c7a9601c9b21a083
Jordi-LG <jordy.leguet@gmail.com>
2020-09-23 13:34:52 +0200

    styles: remove cursor pointer on card

--- src/components/Profile/Story/index.js
+++ src/components/Profile/Story/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Story = ({ id, title, status, handleModalDeleteStory }) => {
   return (
-    <li className="stories__story cursor-pointer mx-auto my-4 h-16 px-4 bg-gray-100 rounded-md flex justify-between items-center">
+    <li className="stories__story mx-auto my-4 h-16 px-4 bg-gray-100 rounded-md flex justify-between items-center">
       <h2 className="stories__story-title overflow-auto h-12 py-3">{title}</h2>
       <div className="stories__story-options flex justify-around items-center w-48">
         <p className="stories__story-status text-xs bg-green-500 h-8 px-1 py-2 w-20 rounded-md m-1 text-center">
