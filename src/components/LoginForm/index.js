05af7ea0c678be2268b0dc6966953d6fe96f1460
Florianargaud <argaud.florian@gmail.com>
2020-09-25 15:04:17 +0200

    fix:img + redesign

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -13,44 +13,54 @@ const LoginForm = ({ handleLogin, isLogged }) => (
           e.preventDefault()
           handleLogin()
         }}
-        className="login-form__panel w-64 flex flex-col justify-center mx-auto p-4 border-2 border-orange-400 bg-orange-900 font-bold"
+        className="login-form__panel w-64 flex flex-col justify-center mx-auto p-4 rounded-md bg-gray-900 shadow-lg font-bold"
       >
-        <h1 className="login-form__title text-xl font-bold text-center text-orange-400">
+        <h1 className="login-form__title text-xl font-bold text-center text-gray-100">
           Prouvez que vous n'êtes pas n'importe quel guerrier.
         </h1>
         <label
-          className="login-form__label mt-4 mb-2 text-orange-400"
+          className="login-form__label mt-4 mb-2 text-gray-100"
           htmlFor="username"
         >
           Nom d'utilisateur
         </label>
-        <Field type="username" name="username" placeholder="Nom d'utilisateur" />
+        <Field
+          type="username"
+          name="username"
+          placeholder="Nom d'utilisateur"
+          className="rounded-md shadow-inner"
+        />
 
-        <label className="login-form my-2 text-orange-400" htmlFor="password">
+        <label className="login-form my-2 text-gray-100" htmlFor="password">
           Mot de passe
         </label>
-        <Field type="password" name="password" placeholder="Mot de passe" />
+        <Field
+          type="password"
+          name="password"
+          placeholder="Mot de passe"
+          className="rounded-md shadow-inner"
+        />
 
         <a
           href="http://ec2-18-234-186-84.compute-1.amazonaws.com/reset-password"
-          className="text-sm underline text-orange-600 hover:text-orange-300"
+          className="text-sm underline text-gray-100 hover:text-orange-300"
         >
           Mot de passe perdu
         </a>
 
         <input
-          className="login-form__submit cursor-pointer h-32 p-6 mt-8 uppercase font-bold whitespace-pre-wrap border-4 border-double border-yellow-400 bg-orange-600 rounded-sm shadow-inner"
+          className="login-form__submit cursor-pointer py-4 px-6 mt-8 uppercase font-bold whitespace-pre-wrap  border-double bg-orange-600  transition duration-150 ease-in rounded-sm shadow-inner hover:bg-orange-500"
           type="submit"
           value="Entrer dans la légende"
         />
       </form>
 
-      <section className="login-form__noaccount w-64 h-32 flex flex-col justify-center mx-auto p-4 border-2 border-orange-400 bg-orange-900 font-bold mt-4 md:mt-0 md:self-center">
+      <section className="login-form__noaccount w-64 h-32 flex flex-col justify-center mx-auto p-4 text-white rounded-md shadow-lg bg-gray-900 font-bold mt-4 md:mt-0 md:self-center">
         <p className="login-form__signup text-center">Pas encore de compte ?</p>
         <Link to="/signup" className="mx-auto">
           <button
             type="button"
-            className="login-form__btn-signup cursor-pointer h-12 px-6 mt-2 uppercase font-bold whitespace-pre-wrap border-4 border-double border-yellow-400 bg-orange-600 rounded-sm shadow-inner"
+            className="login-form__btn-signup cursor-pointer h-12 text-gray-900 px-6 mt-2 uppercase font-bold whitespace-pre-wrap bg-orange-600 rounded-sm shadow-inner"
           >
             Créer un compte
           </button>
