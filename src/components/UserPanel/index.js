a548838780f3f4db858f8d8104e74c80c6081c82
Jordi-LG <jordy.leguet@gmail.com>
2020-11-02 10:50:21 +0100

    fix: display the username after the login success redirection

--- src/components/UserPanel/index.js
+++ src/components/UserPanel/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
 import "./styles.css"
@@ -7,18 +7,19 @@ import { fetchDatasUserPanel } from "src/selectors/nav"
 const UserPanel = ({ isLogged, connectedId, username }) => {
   // datas depends of only when user is connected
   const navListUserData = fetchDatasUserPanel(isLogged, connectedId)
+  const [nameToDisplay, setNameToDisplay] = useState("jeune aventurier")
+
+  useEffect(() => {
+    if (username !== "") {
+      setNameToDisplay(username.charAt(0).toUpperCase() + username.slice(1))
+    }
+  }, [username])
 
   return (
     <div className="user-panel h-24 w-full bg-gray-900 text-white flex items-center justify-between rounded-l-full md:flex-col md:rounded-lg md:relative md:justify-center md:text-center">
       <div className="user-panel__img h-24 w-24 rounded-full shadow-lg bg-contain bg-center md:absolute md:h-48 md:w-48 md:bg-cover" />
       <p className="user-panel__content w-48 ml-4 mr-auto text-xl font-bold md:mt-16 md:mr-0 md:ml-0 md:mb-6">
-        Bienvenue, <br />{" "}
-        {username !== "" ? (
-          <span className="capitalize">{username}</span>
-        ) : (
-          "jeune aventurier"
-        )}
-        .
+        Bienvenue, <br /> <span>{nameToDisplay}.</span>
       </p>
       <div className="user-panel__buttons mr-auto text-xl md:mr-0 md:mt-2 md:flex md:flex-col xl:flex-row xl:justify-evenly xl:w-full xl:h-16 ">
         {navListUserData.map((buttonUser, key) => (
